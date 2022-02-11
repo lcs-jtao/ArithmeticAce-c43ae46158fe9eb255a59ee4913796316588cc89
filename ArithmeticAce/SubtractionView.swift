6cedf31411c0564dac8886ac0a9c3c1862c99d4e
Joyce Tao <jtao@lcs.on.ca>
2022-02-11 15:30:52 -0500

    Create SubtractionView which generated subtraction problems

---
+++ ArithmeticAce/SubtractionView.swift
@@ -0,0 +1,123 @@
+//
+//  SubtractionView.swift
+//  ArithmeticAce
+//
+//  Created by Joyce Tao on 2022-02-11.
+//
+
+import SwiftUI
+
+struct SubtractionView: View {
+    // MARK: Stored properties
+    @State var minuend = Int.random(in: 1...12)
+    @State var subtrahend = Int.random(in: 1...12)
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctProduct: Int {
+        return minuend - subtrahend
+    }
+    
+    var body: some View {
+                
+        VStack(spacing: 0) {
+            HStack {
+                Text("-")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(minuend)")
+                    Text("\(subtrahend)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                        //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.red)
+                        .opacity(answerCorrect == false && answerChecked == true ? 1.0 : 0.0)
+                }
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's not correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .opacity(answerChecked ? 0.0 : 1.0)
+                
+                Button(action: {
+                    
+                    // Generate new numbers
+                    minuend = Int.random(in: 1...12)
+                    subtrahend = Int.random(in: 1...12)
+                    
+                    // Reset properties that we are using to keep track of whether a question has been answered and whether the answer is correct
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the field where the user gives an answer
+                    inputGiven = ""
+                    
+                }, label: {
+                    Text("New Question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    .opacity(answerChecked ? 1.0 : 0.0)
+            }
+
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+    }
+}
+
+struct SubtractionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubtractionView()
+    }
+}
