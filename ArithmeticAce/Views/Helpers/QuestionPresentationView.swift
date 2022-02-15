f385f393563e6ee109fb19b182dea1691e8e0e97
Joyce Tao <jtao@lcs.on.ca>
2022-02-15 09:22:41 -0500

    Applied abstraction to present the values and operation for a question

---
+++ ArithmeticAce/Views/Helpers/QuestionPresentationView.swift
@@ -0,0 +1,39 @@
+//
+//  QuestionPresentationView.swift
+//  ArithmeticAce
+//
+//  Created by Joyce Tao on 2022-02-15.
+//
+
+import SwiftUI
+
+struct QuestionPresentationView: View {
+    
+    // MARK: Stored properties
+    let operation: String
+    let firstValue: Int
+    let secondValue: Int
+    
+    // MARK: Computed properties
+    var body: some View {
+        
+        HStack {
+            
+            Text(operation)
+            
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                Text("\(firstValue)")
+                Text("\(secondValue)")
+            }
+        }
+    }
+}
+
+struct QuestionPresentationView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionPresentationView(operation: "+", firstValue: 10, secondValue: 5)
+    }
+}
+
